0130ce8250c8acc7ace9908391971305652b79fb
Lucas <kyuoven@duck.com>
2022-06-13 13:41:44 +0200

    README.md and First exercise

---
+++ Exercises/Ex1.On_Process.py
@@ -0,0 +1,4 @@
+# 2022-06-13
+
+# homework: - Do research about Scrum, Agile and XP(eXtreme Programming)
+#           - Do the exercise challenge on a .md file (they are just questions)
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # LearningMorePy
+
+Following Zed A. Shaw's 'Learn More Python the Hard Way' book from the 'Hard Way' series. This repo will also count as my lab journal.
